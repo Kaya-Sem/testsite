1843cc7af6bf1b109f5971d6bf769d5c73164d21
Kaya-Sem <kayasemvc@gmail.com>
2025-02-12 17:39:27 +0100

    update

--- main.js
+++ main.js
@@ -2,6 +2,38 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".verdiep_selector_button");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            // Verwijder de "active" klasse van alle knoppen
+            buttons.forEach((btn) => btn.classList.remove("active"));
+
+            // Voeg de "active" klasse toe aan de aangeklikte knop
+            event.target.classList.add("active");
+
+            // Haal de verdieping op en voer een functie uit
+            const selectedFloor = event.target.getAttribute("data-floor");
+            handleButtonClick(selectedFloor);
+        });
+    });
+});
+
+// Voorbeeldfunctie die de geselecteerde verdieping afhandelt
+function handleButtonClick(floor) {
+    console.log(`Geselecteerde verdieping: ${floor}`);
+}
+
+
+
+
+/* THREE JS */
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff); // Set scene background to white
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
